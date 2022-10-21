be32fc6701413f31bb84d72f938ae1897c5aa98d
yukiovo <1050322524@qq.commkdir>
2022-10-21 10:03:18 +0800

    update

--- vite-project/src/components/VideoCard/VideoCard.vue
+++ vite-project/src/components/VideoCard/VideoCard.vue
@@ -4,10 +4,11 @@
             <div class="VideoList">
                 <div class="videoItemWrap">
                     <div class="videoItemContent">
-                        <div class="videoItem" v-for="(item,index) in props.content" :key="item.id">
+                        <div class="videoItem" v-for="(item,index) in props.content" :key="item.id" @click="">
                             <div class="coverWrap">
                                 <div class="cover">
-                                    <img src="../../assets/images/home_img_ycpc.png" alt="" class="coverClass">
+                                    <img src="../../assets/images/home_img_ycpc.png" alt="" class="coverClass"
+                                        v-show="item.momentClassificationBackup.originalStatement===1">
                                     <img :src="item.cover" alt="" class="contentCover">
                                     <img src="../../assets/images/play.png" alt="" class="playIcon">
                                 </div>
@@ -17,7 +18,13 @@
                                     <div class="title">
                                         <div class="example">
                                             <span>
-                                                <img src="https://rs.dance365.com/title_original.svg" alt="">
+                                                <img :src="item.momentClassificationBackup.classification==='original'?
+                                                'https://rs.dance365.com/title_original.svg':''||
+                                                item.momentClassificationBackup.classification==='course'?
+                                                'https://rs.dance365.com/title_course.svg':''||
+                                                item.momentClassificationBackup.classification==='information'?
+                                                'https://rs.dance365.com/title_information1.svg':'' 
+                                                " alt="">
                                             </span>
                                             <a href="">{{item.title}}</a>
                                         </div>
@@ -35,7 +42,7 @@
                                                     d="M621.674667 408.021333c16.618667-74.24 28.224-127.936 34.837333-161.194666C673.152 163.093333 629.941333 85.333333 544.298667 85.333333c-77.226667 0-116.010667 38.378667-138.88 115.093334l-0.586667 2.24c-13.728 62.058667-34.72 110.165333-62.506667 144.586666a158.261333 158.261333 0 0 1-119.733333 58.965334l-21.909333 0.469333C148.437333 407.808 106.666667 450.816 106.666667 503.498667V821.333333c0 64.8 52.106667 117.333333 116.394666 117.333334h412.522667c84.736 0 160.373333-53.568 189.12-133.92l85.696-239.584c21.802667-60.96-9.536-128.202667-70.005333-150.186667a115.552 115.552 0 0 0-39.488-6.954667H621.674667z"
                                                     p-id="20499" fill="#cdcdcd"></path>
                                             </svg>
-                                            <span>25</span>
+                                            <span>{{item.statisticsBackup.praiseCount}}</span>
                                         </span>
                                         <span class="commentCount">
                                             <svg t="1666146659511" class="icon" viewBox="0 0 1035 1024" version="1.1"
@@ -44,7 +51,7 @@
                                                     d="M905.898667 44.032 129.365333 44.032c-71.338667 0-129.365333 58.026667-129.365333 129.365333l0 518.144c0 71.338667 58.026667 129.365333 129.365333 129.365333l253.952 0 134.144 163.84 133.802667-163.84 254.293333 0c71.338667 0 129.365333-58.026667 129.365333-129.365333L1034.922667 173.397333C1035.264 102.058667 977.237333 44.032 905.898667 44.032L905.898667 44.032 905.898667 44.032zM236.885333 498.005333c-34.474667 0-62.464-27.989333-62.464-62.464 0-34.474667 27.989333-62.464 62.464-62.464 34.474667 0 62.464 27.989333 62.464 62.464C299.349333 470.016 271.36 498.005333 236.885333 498.005333L236.885333 498.005333 236.885333 498.005333zM518.144 498.005333c-34.474667 0-62.464-27.989333-62.464-62.464 0-34.474667 27.989333-62.464 62.464-62.464 34.474667 0 62.464 27.989333 62.464 62.464C580.608 470.016 552.618667 498.005333 518.144 498.005333L518.144 498.005333zM799.402667 498.005333c-34.474667 0-62.464-27.989333-62.464-62.464 0-34.474667 27.989333-62.464 62.464-62.464 34.474667 0 62.464 27.989333 62.464 62.464C861.866667 470.016 833.877333 498.005333 799.402667 498.005333L799.402667 498.005333zM799.402667 498.005333"
                                                     p-id="21517" fill="#cdcdcd"></path>
                                             </svg>
-                                            <span>0</span>
+                                            <span>{{item.statisticsBackup.commentCount}}</span>
                                         </span>
                                         <span class="favoriteCount">
                                             <svg t="1666146694561" class="icon" viewBox="0 0 1024 1024" version="1.1"
@@ -53,15 +60,16 @@
                                                     d="M335.008 916.629333c-35.914667 22.314667-82.88 10.773333-104.693333-25.557333a77.333333 77.333333 0 0 1-8.96-57.429333l46.485333-198.24a13.141333 13.141333 0 0 0-4.021333-12.864l-152.16-132.586667c-31.605333-27.52-35.253333-75.648-8.234667-107.733333a75.68 75.68 0 0 1 51.733333-26.752L354.848 339.2c4.352-0.362667 8.245333-3.232 10.026667-7.594667l76.938666-188.170666c16.032-39.2 60.618667-57.92 99.52-41.461334a76.309333 76.309333 0 0 1 40.832 41.461334l76.938667 188.16c1.781333 4.373333 5.674667 7.253333 10.026667 7.605333l199.712 16.277333c41.877333 3.413333 72.885333 40.458667 69.568 82.517334a76.938667 76.938667 0 0 1-26.08 51.978666l-152.16 132.586667c-3.541333 3.082667-5.141333 8.074667-4.021334 12.853333l46.485334 198.24c9.621333 41.013333-15.36 82.336-56.138667 92.224a75.285333 75.285333 0 0 1-57.525333-9.237333l-170.976-106.24a11.296 11.296 0 0 0-12.010667 0l-170.986667 106.24z"
                                                     p-id="22528" fill="#cdcdcd"></path>
                                             </svg>
-                                            <span>101</span>
+                                            <span>{{item.statisticsBackup.favoriteCount}}</span>
                                         </span>
                                     </div>
                                 </div>
                                 <div class="card-info">
                                     <div class="left">
-                                        <img src="../../assets/images/avatar.jpg" alt="" class="avatar">
-                                        <span class="name">城市城市城市城市</span>
-                                        <img src="../../assets/images/vip.png" alt="" class="vip">
+                                        <img :src="item.creatorBackup.avatar" alt="" class="avatar">
+                                        <span class="name">{{item.creatorBackup.name}}</span>
+                                        <img :src="item.creatorBackup.certificationType==='personal'?'../../assets/images/vip.png':''||item.creatorBackup.certificationType==='organization'?'../../assets/images/authenticate_organ@3x.png':''"
+                                            alt="" class="vip" v-show="!item.creatorBackup.certificationType">
                                     </div>
                                     <span class="uploadTime">{{moment(item.onsellTime).format('MM-DD')}}</span>
                                 </div>
@@ -77,6 +85,7 @@
 <script setup lang="ts">
 import moment from 'moment'
 let props = defineProps(['content'])
+console.log(moment(1613318399051).format('MMMM Do YYYY, h:mm:ss'))
 </script>
 
 <style lang="less" scoped>
