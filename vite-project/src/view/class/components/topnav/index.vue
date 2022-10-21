40366b967ed37685761654d3c9339dfa5cb428c8
fuxishan <11168681+fuxishan@user.noreply.gitee.com>
2022-10-21 09:24:19 +0800

    class

--- vite-project/src/view/class/components/topnav/index.vue
+++ vite-project/src/view/class/components/topnav/index.vue
@@ -1,13 +1,43 @@
 <template>
-    <div>
-<h1>头部导航</h1>
-    </div>
+  <div class="vipPart">
+    <el-card>
+      <div class="card-header vip-header">
+        <span>会员专区</span>
+      </div>
+      <div class="vip-topNav">
+        <div class="vip-topNav-item1">
+          <div class="vip-topNav-one vip-topNav-one-border">
+            <div class="vip-toNav-one-sideRight">
+              <span>登录</span>
+              <div>会员登录即享十大特权</div>
+            </div>
+          </div>
+        </div>
+        <div class="vip-topNav-item2">
+          <div class="vip-topNav-one">
+            <img src="https://rs.dance365.com/teach_video_free.png" alt="" />
+            <div class="vip-toNav-one-sideRight">
+              <span>以下<span style="color: red">713</span>个会员专区课程</span>
+              <div>全部免费学习</div>
+            </div>
+          </div>
+        </div>
+        <div class="vip-topNav-item3">
+          <div class="vip-topNav-one">
+            <img src="https://rs.dance365.com/member_preferential.png" alt="" />
+            <div class="vip-toNav-one-sideRight">
+              <span>购买非会员专区课程</span>
+              <div>可获会员立减优惠</div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </el-card>
+  </div>
 </template>
 
 <script setup lang="ts">
-
 </script>
 
 <style scoped>
-
 </style>
