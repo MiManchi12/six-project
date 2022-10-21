a8205201915509f9b3ffbc282f0a3e9279f2e2b5
SoberWLW <1176458601@qq.com>
2022-10-21 21:27:44 +0800

    123

--- vite-project/src/view/home/attention/index.vue
+++ vite-project/src/view/home/attention/index.vue
@@ -1,13 +1,244 @@
 <template>
-    <div>
-
+    <div class="card">
+        <div class="list">
+            <div data-v-5fff7c24="" class="list_item user_mini_card"><span data-v-5fff7c24="" class="avatar"
+                    style="background-image: url(&quot;https://images-la.dance365.com/1495878873716CWMO299253gmg7ry6ru.jpg?imageView2/0/w/200/h/1200/format/webp/ignore-error/1&quot;);"></span>
+                <div data-v-5fff7c24="" class="text_line"><span data-v-5fff7c24="" title="一个跳舞的人" class="name"><span
+                            data-v-5fff7c24="" class="content_card_title">一个跳舞的人</span></span>
+                    <!---->
+                </div>
+                <div data-v-6c9b5798="" data-v-5fff7c24="" class="follow_btn follow-button-container unactive default"
+                    style="width: 70px;">
+                    <!----><svg data-v-6c9b5798="" aria-hidden="true" class="icon" style="width: 10px; height: 12px;">
+                        <use xlink:href="#icon-follow_add"></use>
+                    </svg>
+                    <!---->
+                    关注
+                </div>
+            </div>
+            <div data-v-5fff7c24="" class="list_item user_mini_card"><span data-v-5fff7c24="" class="avatar"
+                    style="background-image: url(&quot;https://rs.everygod.com/photo/07c8c548-600d-42db-a14f-15ffb7d0863c.jpg?imageView2/0/w/200/h/1200/format/webp/ignore-error/1&quot;);"></span>
+                <div data-v-5fff7c24="" class="text_line"><span data-v-5fff7c24="" title="心舞飞漾" class="name"><span
+                            data-v-5fff7c24="" class="content_card_title">心舞飞漾</span></span>
+                    <!---->
+                </div>
+                <div data-v-6c9b5798="" data-v-5fff7c24="" class="follow_btn follow-button-container unactive default"
+                    style="width: 70px;">
+                    <!----><svg data-v-6c9b5798="" aria-hidden="true" class="icon" style="width: 10px; height: 12px;">
+                        <use xlink:href="#icon-follow_add"></use>
+                    </svg>
+                    <!---->
+                    <font style="vertical-align: inherit;">
+                        <font style="vertical-align: inherit;">
+                            关注
+                        </font>
+                    </font>
+                </div>
+            </div>
+            <div data-v-5fff7c24="" class="list_item user_mini_card"><span data-v-5fff7c24="" class="avatar"
+                    style="background-image: url(&quot;https://rs.everygod.com/photo/xxx_1567758460487_1567490915118.png?imageView2/0/w/200/h/1200/format/webp/ignore-error/1&quot;);"></span>
+                <div data-v-5fff7c24="" class="text_line"><span data-v-5fff7c24="" title="舞888" class="name"><span
+                            data-v-5fff7c24="" class="content_card_title">舞888</span></span>
+                    <!---->
+                </div>
+                <div data-v-6c9b5798="" data-v-5fff7c24="" class="follow_btn follow-button-container unactive default"
+                    style="width: 70px;">
+                    <!----><svg data-v-6c9b5798="" aria-hidden="true" class="icon" style="width: 10px; height: 12px;">
+                        <use xlink:href="#icon-follow_add"></use>
+                    </svg>
+                    <!---->
+                    <font style="vertical-align: inherit;">
+                        <font style="vertical-align: inherit;">
+                            关注
+                        </font>
+                    </font>
+                </div>
+            </div>
+            <div data-v-5fff7c24="" class="list_item user_mini_card"><span data-v-5fff7c24="" class="avatar"
+                    style="background-image: url(&quot;https://images-la.dance365.com/IM1520758877551FEIBJMECDEEHGFJEK?imageView2/0/w/200/h/1200/format/webp/ignore-error/1&quot;);"></span>
+                <div data-v-5fff7c24="" class="text_line"><span data-v-5fff7c24="" title="舞蹈说" class="name"><span
+                            data-v-5fff7c24="" class="content_card_title">舞蹈说</span></span>
+                    <!---->
+                </div>
+                <div data-v-6c9b5798="" data-v-5fff7c24="" class="follow_btn follow-button-container unactive default"
+                    style="width: 70px;">
+                    <!----><svg data-v-6c9b5798="" aria-hidden="true" class="icon" style="width: 10px; height: 12px;">
+                        <use xlink:href="#icon-follow_add"></use>
+                    </svg>
+                    <!---->
+                    <font style="vertical-align: inherit;">
+                        <font style="vertical-align: inherit;">
+                            关注
+                        </font>
+                    </font>
+                </div>
+            </div>
+            <div data-v-5fff7c24="" class="list_item user_mini_card"><span data-v-5fff7c24="" class="avatar"
+                    style="background-image: url(&quot;https://rs.everygod.com/photo/900079_1547012470_51b00d4e0f954b7cb9f76c81b86834a0.jpg?imageView2/0/w/200/h/1200/format/webp/ignore-error/1&quot;);"></span>
+                <div data-v-5fff7c24="" class="text_line"><span data-v-5fff7c24="" title="会跳舞的蝈蝈" class="name"><span
+                            data-v-5fff7c24="" class="content_card_title">会跳舞的蝈蝈</span></span>
+                    <!---->
+                </div>
+                <div data-v-6c9b5798="" data-v-5fff7c24="" class="follow_btn follow-button-container unactive default"
+                    style="width: 70px;">
+                    <!----><svg data-v-6c9b5798="" aria-hidden="true" class="icon" style="width: 10px; height: 12px;">
+                        <use xlink:href="#icon-follow_add"></use>
+                    </svg>
+                    <!---->
+                    <font style="vertical-align: inherit;">
+                        <font style="vertical-align: inherit;">
+                            关注
+                        </font>
+                    </font>
+                </div>
+            </div>
+            <div data-v-5fff7c24="" class="list_item user_mini_card"><span data-v-5fff7c24="" class="avatar"
+                    style="background-image: url(&quot;https://rs.everygod.com/photo/xxx_1592292233519_1478158170000?imageView2/0/w/200/h/1200/format/webp/ignore-error/1&quot;);"></span>
+                <div data-v-5fff7c24="" class="text_line"><span data-v-5fff7c24="" title="舞蹈资料君" class="name"><span
+                            data-v-5fff7c24="" class="content_card_title">舞蹈资料君</span></span>
+                    <!---->
+                </div>
+                <div data-v-6c9b5798="" data-v-5fff7c24="" class="follow_btn follow-button-container unactive default"
+                    style="width: 70px;">
+                    <!----><svg data-v-6c9b5798="" aria-hidden="true" class="icon" style="width: 10px; height: 12px;">
+                        <use xlink:href="#icon-follow_add"></use>
+                    </svg>
+                    <!---->
+                    <font style="vertical-align: inherit;">
+                        <font style="vertical-align: inherit;">
+                            关注
+                        </font>
+                    </font>
+                </div>
+            </div>
+            <div data-v-5fff7c24="" class="list_item user_mini_card"><span data-v-5fff7c24="" class="avatar"
+                    style="background-image: url(&quot;https://rs.everygod.com/photo/708436_1545273041_cfdc62a508954da1b51a6f787e4abc23.jpg?imageView2/0/w/200/h/1200/format/webp/ignore-error/1&quot;);"></span>
+                <div data-v-5fff7c24="" class="text_line"><span data-v-5fff7c24="" title="花为裙兮舞为裳" class="name"><span
+                            data-v-5fff7c24="" class="content_card_title">花为裙兮舞为裳</span></span>
+                    <!---->
+                </div>
+                <div data-v-6c9b5798="" data-v-5fff7c24="" class="follow_btn follow-button-container unactive default"
+                    style="width: 70px;">
+                    <!----><svg data-v-6c9b5798="" aria-hidden="true" class="icon" style="width: 10px; height: 12px;">
+                        <use xlink:href="#icon-follow_add"></use>
+                    </svg>
+                    <!---->
+                    <font style="vertical-align: inherit;">
+                        <font style="vertical-align: inherit;">
+                            关注
+                        </font>
+                    </font>
+                </div>
+            </div>
+            <div data-v-5fff7c24="" class="list_item user_mini_card"><span data-v-5fff7c24="" class="avatar"
+                    style="background-image: url(&quot;https://images-la.dance365.com/1502174456526KYFJ678699014797531?imageView2/0/w/200/h/1200/format/webp/ignore-error/1&quot;);"></span>
+                <div data-v-5fff7c24="" class="text_line"><span data-v-5fff7c24="" title="习舞女汉子" class="name"><span
+                            data-v-5fff7c24="" class="content_card_title">习舞女汉子</span></span>
+                    <!---->
+                </div>
+                <div data-v-6c9b5798="" data-v-5fff7c24="" class="follow_btn follow-button-container unactive default"
+                    style="width: 70px;">
+                    <!----><svg data-v-6c9b5798="" aria-hidden="true" class="icon" style="width: 10px; height: 12px;">
+                        <use xlink:href="#icon-follow_add"></use>
+                    </svg>
+                    <!---->
+                    <font style="vertical-align: inherit;">
+                        <font style="vertical-align: inherit;">
+                            关注
+                        </font>
+                    </font>
+                </div>
+            </div>
+        </div>
     </div>
 </template>
+  
 
 <script setup lang="ts">
 
 </script>
 
 <style scoped>
+.card {
+    width: 100%;
+}
+.list {
+    width: 1200px;
+    height: 160px;
+    margin: auto;
+    display: -webkit-box;
+    display: -ms-flexbox;
+    display: flex;
+    -webkit-box-pack: start;
+    -ms-flex-pack: start;
+    justify-content: flex-start;
+    background: #f5f7f9;
+}
 
+.recommend_follow_wrapper .list .list_item:not(:last-child) {
+    margin-right: 10px;
+}
+
+.list_item.user_mini_card .avatar {
+    background: #c3c3c3;
+    border-radius: 50%;
+    width: 60px;
+    height: 60px;
+    display: inline-block;
+    margin-bottom: 6px;
+    background-size: cover;
+    background-position: 50%;
+    cursor: pointer;
+}
+
+.list_item.user_mini_card {
+    margin-right: 10px;
+    text-align: center;
+    -webkit-box-sizing: border-box;
+    background: #fff;
+    color: #7d8090;
+    font-size: 14px;
+    width: 142px;
+    height: 160px;
+    padding: 16px 12px;
+    box-sizing: border-box;
+}
+
+.list_item.user_mini_card .follow_btn {
+    -webkit-box-sizing: border-box;
+    box-sizing: border-box;
+    margin-top: 6px;
+    font-size: 12px;
+    height: 35px;
+    padding: 0;
+    line-height: 35px;
+}
+.default[data-v-6c9b5798]:hover {
+    background-color: rgba(249,54,132,.2);
+}
+.default[data-v-6c9b5798] {
+    color: #f93684;
+    background-color: #f9ebf1;
+    border-radius: 20px;
+    font-size: 14px;
+    text-align: center;
+    padding: 10px 0;
+    border: 1px solid #f9ebf1;
+    width: 86px;
+    display: inline-block;
+    cursor: pointer;
+}
+/* .default.unactive .icon {
+    -webkit-transform: translateY(-1px);
+    transform: translateY(-1px);
+}
+.icon, .icon svg {
+    overflow: visible;
+} */
+.icon {
+    width: 1em;
+    height: 1em;
+    vertical-align: -0.15em;
+    fill: currentColor;
+}
 </style>
